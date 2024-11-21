ec15b1493cd4b37cdc050b23ac779ea9108cbe4e
Patrick Hu <phhu@ucsc.edu>
2024-11-20 17:56:44 -0800

    Updated vite config and html

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: 'CMPM121-Final', // e.g., /CMPM121-Final/
   build: {
-    outDir: 'dist', // Ensure the output directory is set for GitHub Pages
-    rollupOptions: {
-      input: 'index.html', // Entry point for the build
-    },
+    outDir: 'dist',
   },
 });
