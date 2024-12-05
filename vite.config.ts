dcd49ca5071e599d0115b28023fc763a85388e58
edmfong <97255519+edmfong@users.noreply.github.com>
2024-12-05 02:07:57 -0800

    attempting to fix pathing issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: 'CMPM121-Final', // e.g., /CMPM121-Final/
+  base: '/CMPM121-Final/', // e.g., /CMPM121-Final/
   build: {
     outDir: 'dist',
   },
